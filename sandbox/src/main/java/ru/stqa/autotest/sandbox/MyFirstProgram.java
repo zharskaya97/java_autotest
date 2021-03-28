0217f73152bb26c30e2d055cabd1c38be8e58295
zharskaya97 <73550021+zharskaya97@users.noreply.github.com>
2021-03-28 21:53:53 +0400

    добавлены новые классы

--- sandbox/src/main/java/ru/stqa/autotest/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/autotest/sandbox/MyFirstProgram.java
@@ -7,23 +7,15 @@ public class MyFirstProgram {
     hello("user");
     hello("Marina");
 
-    double len = 5;
-    System.out.println("Площадь квадрата со стороной " + len + " = " + area(len));
+    Square s = new Square(5);
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
-    double a = 4;
-    double b = 6;
-    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a,b));
+    Rectangle r = new Rectangle(4, 6);
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
   }
 
   public static void hello(String somebody) {
     System.out.println("Hello, " + somebody + "!");
   }
 
-  public static double area(double len){
-    return len * len;
-  }
-
-  public static double area(double a, double b){
-    return a*b;
-  }
 }
---
+++ sandbox/src/main/java/ru/stqa/autotest/sandbox/Rectangle.java
@@ -0,0 +1,16 @@
+package ru.stqa.autotest.sandbox;
+
+public class Rectangle {
+
+  public double a;
+  public double b;
+
+  public Rectangle(double a, double b) {
+    this.a = a;
+    this.b = b;
+  }
+
+  public double area(){
+    return this.a * this.b;
+  }
+}
